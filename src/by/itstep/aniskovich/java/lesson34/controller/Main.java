b04514d1507eb17b18676318e246f74ea4cf7745
Ycanzzay <steam3bera228@gmail.com>
2024-03-21 20:38:28 +0300

    add WeekDayType enum

--- src/by/itstep/aniskovich/java/lesson34/controller/Main.java
+++ src/by/itstep/aniskovich/java/lesson34/controller/Main.java
@@ -1,20 +1,20 @@
 package by.itstep.aniskovich.java.lesson34.controller;
 
 import by.itstep.aniskovich.java.lesson34.model.WeekDay;
+import by.itstep.aniskovich.java.lesson34.model.WeekDayType;
 
 public class Main {
     public static void main(String[] args) {
-        WeekDay monday1 = WeekDay.MONDAY;
-        WeekDay monday2 = WeekDay.MONDAY;
-        WeekDay monday3 = WeekDay.MONDAY;
-        WeekDay monday4 = WeekDay.MONDAY;
-
-//        WeekDay friday = new WeekDay("Friday", 5);
+//        WeekDay monday1 = WeekDay.MONDAY;
+//        WeekDay monday2 = WeekDay.MONDAY;
+//        WeekDay monday3 = WeekDay.MONDAY;
+//        WeekDay monday4 = WeekDay.MONDAY;
 //
+////        WeekDay friday = new WeekDay("Friday", 5);
+////
 //        WeekDay mars = new WeekDay("Mars", 8); // (1)!!!
-
-        System.out.println(monday1);
-        System.out.println(WeekDay.FRIDAY);
+        System.out.println(WeekDayType.THURSDAY);
+//        System.out.println(WeekDay.FRIDAY);
 //        System.out.println(friday);
     }
 
---
+++ src/by/itstep/aniskovich/java/lesson34/model/WeekDayType.java
@@ -0,0 +1,30 @@
+package by.itstep.aniskovich.java.lesson34.model;
+
+public enum WeekDayType {
+    MONDAY("Monday", 1),
+    TUESDAY("Tuesday", 2),
+    WEDNESDAY("Wednesday",3),
+    THURSDAY("Thursday", 4),
+    FRIDAY("Friday", 5),
+    SATURDAY("Saturday", 6),
+    SUNDAY("Sunday", 7);
+
+    private final String name;
+    private final int number;
+
+    WeekDayType() {
+        System.out.println("object...");
+        name = "Monday";
+        number = 1;
+    }
+
+    WeekDayType(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return name + "[" + number + "]";
+    }
+}
